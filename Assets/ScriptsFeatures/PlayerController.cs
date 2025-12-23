02e1d180ea2f01fea9ae6d813c6b4f5be7476bb1
NotEnoughMax <maxstream282@gmail.com>
2025-12-23 11:26:49 +0200

    Added jump to player

--- Assets/ScriptsFeatures/PlayerController.cs
+++ Assets/ScriptsFeatures/PlayerController.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float jumpForce = 5f;
     public float mouseSensitivity = 2f;
     public Transform cameraHolder;
 
     private CharacterController controller;
     private float verticalRotation = 0f;
+
+
 
     void Start()
     {
